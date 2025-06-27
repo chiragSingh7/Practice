6f728701a158e9db47f0118b65f657a15372f30d
chiragSingh7 <rathorechirag1212@gmail.com>
2025-06-27 09:28:46 +0530

    OOPS Practice

---
+++ InternPractice/src/Main_Practice.java
@@ -0,0 +1,15 @@
+public class Main_Practice {
+
+    public static void main(String[] args) {
+
+       fish fish = new fish();
+       rabbit rabbit = new rabbit();
+       eagle eagle = new eagle();
+
+       rabbit.flee();
+       eagle.hunt();
+       fish.flee();
+       fish.hunt();
+
+    }
+}
