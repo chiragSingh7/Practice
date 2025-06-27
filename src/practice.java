1439bdd2831a55a7b925d907884d5075f7feac5f
chiragSingh7 <rathorechirag1212@gmail.com>
2025-06-27 09:35:05 +0530

    Practice

---
+++ src/practice.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class practice{
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the string you want to remove duplicates from: ");
+        String s = scanner.nextLine();
+
+        int n = s.length();
+        int[] arr = new int[26];
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0 ; i<n ; i++){
+            if(arr[s.charAt(i) - 'a'] == 0){
+                arr[s.charAt(i) - 'a']++;
+                result.append(s.charAt(i));
+            }
+        }
+
+        System.out.println(result.toString());
+
+        scanner.close();
+    }
+}
